26bc99c68ffc35ce9e9e9f2d3ac8c96fb825adb6
Alberto Flores <alberto.floreso94@gmail.com>
2021-10-08 14:33:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello my server on express');
+});
+
+app.get('/new-route', (req, res) => {
+  res.send('Hello, im a new route');
+});
+
+routerApi(app);
+
+app.listen(port, () => {
+  console.log(`My port ${port}`)
+});
